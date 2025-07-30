27f4499ea3e3d0fdefa77ad05e330fb1bf78552b
Okosa Chibuikem Joshua <okosachibuikem69@gmail.com>
2025-07-30 01:51:25 +0100

    another

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// Select DOM elements
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
+const menuBar = document.querySelector('.menu');
+const container = document.getElementById('container');
+
+
+
+// Show the menu
+hamburger.addEventListener('click', () => {
+  menuBar.classList.add('show');
+});
+
+// Close the menu
+close.addEventListener('click', () => {
+  menuBar.classList.remove('show');
+});
+
+// Close the menu when a link is clicked
+menuBar.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => {
+    menuBar.classList.remove('show');
+  });
+});
+
+// Close the menu when clicking outside of it
+document.addEventListener('click', (event) => {
+  const isClickInside = menuBar.contains(event.target) || hamburger.contains(event.target);
+  if (!isClickInside) {
+    menuBar.classList.remove('show');
+  }
+});
+
+
+
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
